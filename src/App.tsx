9daa1f8b7ab7ae98f93abd91aca6b4a70e6ea556
Shruti Rastogi <shruti.rastogi2501@gmail.com>
2025-02-25 19:25:37 +0100

    Objective 2 completed

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@ import { Routes, Route } from "react-router";
 
 import Layout from "@/app/dashboard/page";
 import Testpage from "@/app/testpage/page";
-import Newpage from '@/app/newpage/page';
+import Newpage from "@/app/newpage/page";
 
 function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<Layout />}>
-                <Route index element={<Testpage />} />
-            </Route>
-            <Route path="/newpage" element ={<Layout/>}>
-                <Route index element={<Newpage/>}/>
-            </Route>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Testpage />} />
+      </Route>
+      <Route path="/newpage" element={<Layout />}>
+        <Route index element={<Newpage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
